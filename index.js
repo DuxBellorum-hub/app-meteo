f5976773ba963aefabf6b23fa09812766ade7a83
dux bellorum <chellah.ibra@gmail.com>
2023-05-14 16:14:38 +0200

    correction position initial

--- index.js
+++ index.js
@@ -18,31 +18,20 @@ const geocoder = NodeGeocoder(options);
 const url = `https://api.tutiempo.net/json/?lan=fr&apid=${process.env.API_KEY}&ll=`;
 
 app.get('/', async (req, res) => {
-  const location = req.ip;
-  await axios(`http://ip-api.com/json/${location}`)
-    .then(async (result) => {
-      const city = result.data.city;
-      const latitude = result.data.lat;
-      const longitude = result.data.lon;
-      const response = await axios.get(url + latitude + ',' + longitude);
-      const data = response.data;
-      console.log(response.data)
-      const hourDataJson = JSON.parse(JSON.stringify(data.hour_hour));
-      let arrHours = Object.values(hourDataJson);
-      const dayTwo = getDay(data.day2.date);
-      const dayThree = getDay(data.day3.date);
-      const dayFour = getDay(data.day4.date);
-      const dayFive = getDay(data.day5.date);
-      const daySix = getDay(data.day6.date);
-      const daySeven = getDay(data.day7.date);
-      let key = process.env.MAP_KEY.toString();
-      res.render("searchBar.twig", { key, data, arrHours, dayTwo, dayThree, dayFour, dayFive, daySix, daySeven, key });
-    })
-    .catch(e => {
-      console.log(e);
-      res.json({"err": e.message});
-    })
-
+  const latitude = "48.866667";
+  const longitude = "2.333333";
+  const response = await axios.get(url + latitude + ',' + longitude);
+  const data = response.data;
+  const hourDataJson = JSON.parse(JSON.stringify(data.hour_hour));
+  let arrHours = Object.values(hourDataJson);
+  const dayTwo = getDay(data.day2.date);
+  const dayThree = getDay(data.day3.date);
+  const dayFour = getDay(data.day4.date);
+  const dayFive = getDay(data.day5.date);
+  const daySix = getDay(data.day6.date);
+  const daySeven = getDay(data.day7.date);
+  let key = process.env.MAP_KEY.toString();
+  res.render("searchBar.twig", { key, data, arrHours, dayTwo, dayThree, dayFour, dayFive, daySix, daySeven, key });
 
 });
 
@@ -52,7 +41,6 @@ app.get('/:id', (req, res) => {
     .then(async (data) => {
       const latitude = data[0].latitude;
       const longitude = data[0].longitude;
-      console.log(latitude, longitude)
       try {
         const response = await axios.get(url + latitude + ',' + longitude);
         const data = response.data;
@@ -66,8 +54,6 @@ app.get('/:id', (req, res) => {
         const daySeven = getDay(data.day7.date);
         let key = process.env.MAP_KEY.toString();
         res.render("index.twig", { data, arrHours, dayTwo, dayThree, dayFour, dayFive, daySix, daySeven, key });
-
-        console.log(data)
       } catch (error) {
         console.error(error);
         res.json(error.message);
